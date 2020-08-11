88a36107c4d27cba332aa4ef78c91d6e241900ee
vippivivpiv <nguyendinhkhanh1997@gmail.com>
2020-08-11 20:57:35 +0700

    Delete Old File

---
+++ Assets/DoNotExit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotExit : MonoBehaviour
+{
+    public GameObject circle;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Exit!! Please Go Back!!");
+        circle.transform.position = new Vector3(0, 0, 0);
+    }
+}
